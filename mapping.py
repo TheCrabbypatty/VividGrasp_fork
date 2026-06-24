f6b023af1765e7aa2a50bbf4bf7b7d53ecec5b1f
Davor Fortuna <145595632+davorfortuna@users.noreply.github.com>
2026-06-24 23:48:24 +0200

    Fix argument ordering issue in convertCoordinates

--- mapping.py
+++ mapping.py
@@ -1,17 +1,10 @@
-# asuming the image is 200 by 200 pixels and the real world dimensions are 1 by 1 meters(it says so in the colab notebook from canvas):
-
-def convertCoordinates(px, py, imgW=200, imgH=200):
+def convertCoordinates(px, py, min_x, max_x, min_y, max_y, imgW=200, imgH=200):
 
     px = max(0, min(px, imgW))
     py = max(0, min(py, imgH))
 
     norm_x= px/imgW
     norm_y= py/imgH
-
-    min_x= -0.5
-    max_x= 0.5
-    min_y= -0.5
-    max_y= 0.5
 
     world_x= min_x + norm_x*(max_x-min_x)
     world_y= max_y - norm_y*(max_y-min_y)
