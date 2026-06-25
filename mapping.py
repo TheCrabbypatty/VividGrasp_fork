b39636d39ab46a87e6e5de7cbcbaf9a982026f75
Davor Fortuna <145595632+davorfortuna@users.noreply.github.com>
2026-06-25 13:09:21 +0200

    Fix argument ordering issue in convertCoordinates

--- mapping.py
+++ mapping.py
@@ -1,4 +1,4 @@
-def convertCoordinates(px, py, world_z, min_x, max_x, min_y, max_y, imgW=200, imgH=200):
+def convertCoordinates(px, py, ball_radius, min_x, max_x, min_y, max_y, imgW=200, imgH=200):
 
     px = max(0, min(px, imgW))
     py = max(0, min(py, imgH))
@@ -8,5 +8,5 @@ def convertCoordinates(px, py, world_z, min_x, max_x, min_y, max_y, imgW=200, im
 
     world_x= min_x + norm_x*(max_x-min_x)
     world_y= max_y - norm_y*(max_y-min_y)
-    targetXYZ=[world_x, world_y, world_z]
+    targetXYZ=[world_x, world_y, ball_radius]
     return (targetXYZ)
